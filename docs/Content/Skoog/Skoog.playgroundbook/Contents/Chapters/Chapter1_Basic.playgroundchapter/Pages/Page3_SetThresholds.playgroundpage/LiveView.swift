2229c97b35868f26e19b694e9457e2c8639cd655
Keith Nagle <keith@skoogmusic.com>
2019-09-30 15:37:13 +0100

    added missing files from previous commit

--- docs/Content/Skoog/Skoog.playgroundbook/Contents/Chapters/Chapter1_Basic.playgroundchapter/Pages/Page3_SetThresholds.playgroundpage/LiveView.swift
+++
@@ -1,9 +0,0 @@
-//
-// Copyright © 2017 Skoogmusic Inc.  All rights reserved.
-//
-import UIKit
-import PlaygroundSupport
-
-let viewController = LiveViewController()
-viewController.setBackgroundGradient(gradient: .gradient6)
-PlaygroundPage.current.liveView = viewController
--- docs/Content/Skoog/Skoog.playgroundbook/Contents/PrivateResources/LinearGradient.fsh
+++
@@ -1,4 +0,0 @@
-void main() {
-    float diag = 0.5 * ((1.0 - v_tex_coord.x) + v_tex_coord.y);
-    gl_FragColor = mix(endColor, startColor, diag);
-}
